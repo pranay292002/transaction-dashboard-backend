38da6f846d0eacbbe6f7613aa8bfee5dc8cb828a
pranay292002 <pranayparbate2002@gmail.com>
2024-10-23 13:28:27 +0530

    Binding port

--- src/index.js
+++ src/index.js
@@ -12,5 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/transactions", transactionRoutes);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000; 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
