673578f9287cfab9d338a18919e2db3e4fb548b3
Pranay Parbate <76903075+pranay292002@users.noreply.github.com>
2024-10-30 22:25:29 +0530

    Update db.js
    added comment elaborated comment

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(`${process.env.MONGO_URI}roxiler_assignment`);
     //process.env.MONGO_URI = mongodb+srv://root:Pranay%4029@cluster0.z1q35.mongodb.net/
+    // I am aware that exposing database links or any other secret API keys is unprofessional and can lead to security issues but I have written this for your easy access
     
     console.log("MongoDB connected");
   } catch (error) {
